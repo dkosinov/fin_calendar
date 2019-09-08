829b7fd24bdd2d7afc3037b9c84edc2ae40001fd
dkosinov <dkosinov@mail.ru>
2019-09-08 15:08:25 +0300

    Таблица строится

--- js/mainTabComponent.js
+++ js/mainTabComponent.js
@@ -96,29 +96,76 @@ Vue.component('mainTab',{
             ],
         }
     },
-    template: `<div class="main-tab-container">
-                    <div class="main-tab">
-                            <table border="1">
-                                <tr class="title-row">
-                                    <th rowspan="2">Статья/Контрагент</th>
-                                    <span></span>
-                                    <th v-for="(period, index) of periods"
-                                        :key="index"
-                                        colspan="8">
-                                        {{period}}
-                                    </th>
-                                </tr>
-                                <tr v-for="n of periods.length"
-                                        class="title-row">
-                                    <th>Сальдо н.п.</th>
-                                    <th>План</th>
-                                    <th>Оплачено</th>
-                                    <th>Зачтено</th>
-                                    <th>Откл. от плана</th>
-                                    <th>Задолженность</th>
-                                    <th>Начислено</th>
-                                    <th>Сальдо к.п.</th>
-                                </tr>
+    template: `<div class="main-table-container">
+                   <div class="flex-container">
+                        <div v-for="(period, index) of periods">
+                            <div v-if="index == 0"
+                                 class="grid-title-container-1">
+                                 <div class="title-row-name">Статья/Контрагент</div>
+                                <div class="title-row-period">{{period}}</div>
+                                <div class="title-row-data">Сальдо н.п.</div>
+                                <div class="title-row-data">План</div>
+                                <div class="title-row-data">Оплачено</div>
+                                <div class="title-row-data">Зачтено</div>
+                                <div class="title-row-data">Отклонение<br>от плана</div>
+                                <div class="title-row-data">Задолжен-<br>ность</div>
+                                <div class="title-row-data">Начислено</div>
+                                <div class="title-row-data">Сальдо к.п.</div>
+                            </div>
+                            <div v-else class="grid-title-container-2">
+                                 <div class="title-row-name-empty"></div>
+                                <div class="title-row-period">{{period}}</div>
+                                <div class="title-row-data">Сальдо н.п.</div>
+                                <div class="title-row-data">План</div>
+                                <div class="title-row-data">Оплачено</div>
+                                <div class="title-row-data">Зачтено</div>
+                                <div class="title-row-data">Отклонение<br>от плана</div>
+                                <div class="title-row-data">Задолжен-<br>ность</div>
+                                <div class="title-row-data">Начислено</div>
+                                <div class="title-row-data">Сальдо к.п.</div>
+                            </div>
+                        </div>
+                   </div>
+                    <div v-for="(org, index) of tableData"
+                        :key="index"
+                        class="flex-container">
+                        <span><div class="data-row-name">{{org.name}}</div></span>
+                        <div v-for="(orgData, index) of org.data"
+                            :key="index"
+                            class="flex-container">                                                   
+                            <div class="data-row-data">{{orgData.debtStart}}</div>
+                            <div class="data-row-data">{{orgData.plan}}</div>
+                            <div class="data-row-data">{{orgData.payment}}</div>
+                            <div class="data-row-data">{{orgData.zachet}}</div>
+                            <div class="data-row-data">{{orgData.plan - orgData.payment - orgData.zachet}}</div>
+                            <div class="data-row-data">{{orgData.debtStart - orgData.payment - orgData.zachet}}</div>
+                            <div class="data-row-data">{{orgData.invoice}}</div>
+                            <div class="data-row-data">{{orgData.debtStart - orgData.payment - orgData.zachet + orgData.invoice}}</div>
+                        </div>
+                    </div>
+ 
+<!--                    <div class="main-tab">-->
+<!--                            <table border="1">-->
+<!--                                <tr class="title-row">-->
+<!--                                    <th rowspan="2">Статья/Контрагент</th>-->
+<!--                                    <span></span>-->
+<!--                                    <th v-for="(period, index) of periods"-->
+<!--                                        :key="index"-->
+<!--                                        colspan="8">-->
+<!--                                        {{period}}-->
+<!--                                    </th>-->
+<!--                                </tr>-->
+<!--                                <tr v-for="n of periods.length"-->
+<!--                                        class="title-row">-->
+<!--                                    <th>Сальдо н.п.</th>-->
+<!--                                    <th>План</th>-->
+<!--                                    <th>Оплачено</th>-->
+<!--                                    <th>Зачтено</th>-->
+<!--                                    <th>Откл. от плана</th>-->
+<!--                                    <th>Задолженность</th>-->
+<!--                                    <th>Начислено</th>-->
+<!--                                    <th>Сальдо к.п.</th>-->
+<!--                                </tr>-->
 <!--                                <tr class="data-row">-->
 <!--                                    <td class="td-first">{{org.name}}</td>-->
 <!--                                    <td class="td-data">{{org.debtStart}}</td>-->
@@ -130,7 +177,7 @@ Vue.component('mainTab',{
 <!--                                    <td class="td-data">{{org.invoice}}</td>-->
 <!--                                    <td class="td-data">{{org.debtStart - org.payment - org.zachet + org.invoice}}</td>-->
 <!--                                </tr>-->
-                            </table>
-                        </div>
+<!--                            </table>-->
+<!--                        </div>-->
                     </div>`
 })
